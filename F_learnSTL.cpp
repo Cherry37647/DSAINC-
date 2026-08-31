142364e272d2bde5dfc291d26ce2a176077eff48
Suman <cherrysuman44@gmail.com>
2026-08-31 19:15:47 +0530

    implementation of stack and queue and set function

--- F_learnSTL.cpp
+++ F_learnSTL.cpp
@@ -1,5 +1,6 @@
 //Algorithm container(vector,queue,set and map) function and iterators
-//pair vector iterators
+//pair vector iterators list deque stack queue priority ueue min heap set multiset unorderedset
+//map multimap extra
 #include<iostream>
 #include<utility>
 #include<vector>
@@ -14,7 +15,6 @@ void explainpair(){
     cout<<arr[0].first<<" "<<arr[1].second<<endl;
  
     pair<int, pair<int, int>> pec = {1, {3, 6}};
-
 // Accessing elements:
 cout << pec.first<<endl;         // Prints 1
 cout << pec.second.first<<endl;  // Prints 3
@@ -23,23 +23,22 @@ cout << pec.second.second<<endl; // Prints 6
 }
 
 void explainvector(){
-
+   //vector v
     vector <int> v;
     v.push_back(1);
     v.emplace_back(2);
-
+    //vector vec
     vector <pair<int,int>> vec;
     vec.push_back({5,7});
     vec.emplace_back(9,3);
-    // cout<<v[0]<<" "<< vec[1].second<<" "<<v[1]<<endl;
-    // cout<<vec[0].second<<" "<<vec[1].first<<endl;
-
-    // vector <int> v1(5,100);
-    // v1.push_back(4);
-    // cout<<v1[0]<<" "<<v1[2]<<" "<<v1[6]<<endl;
+    cout<<v[0]<<" "<< vec[1].second<<" "<<v[1]<<endl;
+    cout<<vec[0].second<<" "<<vec[1].first<<endl;
+//vector v1
+    vector <int> v1(5,100);
+    v1.push_back(4);
+    cout<<v1[0]<<" "<<v1[2]<<" "<<v1[6]<<endl;
 
     //it points to memory address while *it points to that element
-    
     vector <int>::iterator it=v.begin();//v.begin point first element of v vector
     it++;//move to second element
     cout<<*it<<endl;//v=[1,2]  v[0]=1 v[1]=2//print 1 index value
@@ -158,11 +157,78 @@ void explainqueue(){
     qu.emplace(3);//{9,3}
     qu.push(8);//{9,3,8}
     qu.pop();//{3,8}
-    qu.back()+=3;
-    cout<<qu.back();//{3,11}
-    cout<<qu.front();
+    qu.back()+=3;//{3,11}
+    cout<<qu.back()<<endl;
+    cout<<qu.front()<<endl;
 
 }
+//push,pop time complexity is o(logn)
+//top 0(1)
+
+//priority queue - arrange element according to value greatest element at top and lowest value element is at bottom follow fifo orde
+void explainpriority_queue(){
+    //maximum heap
+    priority_queue <int> pq;
+    pq.push(3);
+    pq.push(9);//{9,3}
+    pq.emplace(3);//{9,3,3}
+    pq.push(8);//{9,8,3,3}
+    cout<<pq.top()<<endl;
+    pq.pop();//{8,3,3}
+    cout<<pq.top()<<endl;
+    
+    //minimum heap
+    priority_queue <int,vector<int>,greater<int>> mh;
+     mh.push(3);
+    mh.push(9);//{3,9}
+    mh.emplace(3);//{3,3,9}
+    mh.push(8);//{3,3,8,9}
+    cout<<mh.top()<<endl;//3
+    mh.pop();//{3,8,9}
+    cout<<mh.top()<<endl;//3
+
+}
+
+
+//set is sorted and unique
+void explainset(){
+    set <int> se;
+    //se.push(); not push and pop work
+    se.insert(4);
+    se.emplace(6);//{4,6}
+    se.emplace(4);//{4,6}
+    se.insert(3);//{3,4,6}
+    auto it =se.find(7);//print last index 
+    cout<<*it<<endl;
+    cout<<se.erase(4)<<endl;//display 0,1 if have then erase and result is 1 otw 0
+    cout<<se.count(1)<<endl;//0 or 1 have 1 or not have 0
+    cout<<se.count(3)<<endl;
+    cout<<se.size()<<endl;
+    se.lower_bound(7);
+    se.upper_bound(6);
+
+}
+
+//multiset is sorted but not unique
+//insert erase count find
+void explainmultiset(){
+    multiset <int> ms;
+    ms.insert(7);
+    ms.insert(8);//{7,8}
+    ms.insert(3);//{3,7,8}
+    ms.insert(7);//{3,7,7,8}
+    cout<<ms.erase(4)<<endl;
+    cout<<ms.empty()<<endl;
+    auto it =ms.find(4);
+    cout<<*it<<endl;
+    ms.erase(ms.find(8));
+    int cnt =ms.count(7);
+    cout<<cnt<<endl;
+    ms.erase(ms.find(7));
+    int cntt =ms.count(7);
+    cout<<cntt<<endl;
+}
+
 
 int main(){
     //explainpair();
@@ -170,6 +236,9 @@ int main(){
     //explainlist();
     //explaindeque();
     //explainstack();
-    explainqueue();
+    //explainqueue();
+    //explainpriority_queue();
+    //explainset();
+    explainmultiset();
     return 0;
 }
