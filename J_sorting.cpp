ba34559b7cfc3a9bbefc0b875da2d975a522573d
Abhishek Rana <a.r.abhishek422@gmail.com>
2026-09-04 23:42:45 +0530

    all sort

--- J_sorting.cpp
+++ J_sorting.cpp
@@ -15,13 +15,140 @@ void selection_sort(int arr[],int n){
 
     }
 }
+void bubble_sort(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        int didswap=0;
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                didswap=1; 
+            }
+
+        }
+        if(didswap==0){
+            break;
+        }
+        cout<<"swap"<<endl;
+    }
+}
+void insertion_sort(int arr[],int n){
+    for(int i=0;i<n-1;i++){
+        int j=i;
+        while(j>0 &&arr[j-1]<arr[j]){
+            int temp=arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+        }
+        j--;
+    }
+}
+
+void merge(int arr[], int low, int mid, int high)
+{
+    int temp[100];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while(i <= mid && j <= high)
+    {
+        if(arr[i] < arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i <= mid)
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while(j <= high)
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for(i = low, k = 0; i <= high; i++, k++)
+    {
+        arr[i] = temp[k];
+    }
+}
+
+void mergeSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int mid = (low + high) / 2;
+
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        merge(arr, low, mid, high);
+    }
+}
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++)
+    {
+        if(arr[j] < pivot)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
 int main(){
     int n;
+    cout<<"Enter the number of integer value to be insert"<<endl;
     cin>>n;
     int arr[n];
+    cout<<"enter the value to be insert"<<endl;
     for(int i=0;i<n;i++)
     cin>>arr[i];
-    selection_sort(arr,n);
+    // selection_sort(arr,n);
+    //bubble_sort(arr, n);
+    //insertion_sort(arr,n);
+    //mergeSort(arr,0,n-1);
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
